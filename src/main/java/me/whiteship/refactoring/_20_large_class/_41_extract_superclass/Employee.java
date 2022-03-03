6b541c49e35ad0a014f1927a9767b582f36f0d59
Kyeong Jun <v49011591@gmail.com>
2022-03-03 21:39:31 +0900

    냄새 20. 거대한 클래스     리팩토링 41. 슈퍼 클래스 추출하기

--- src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
+++ src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
@@ -1,12 +1,14 @@
 package me.whiteship.refactoring._20_large_class._41_extract_superclass;
 
-public class Employee {
+public class Employee extends Party {
 
     private Integer id;
 
-    private String name;
-
     private double monthlyCost;
+
+    public Employee(String name) {
+        super(name);
+    }
 
     public double annualCost() {
         return this.monthlyCost * 12;
@@ -16,11 +18,8 @@ public class Employee {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getMonthlyCost() {
+    @Override
+    public double monthlyCost() {
         return monthlyCost;
     }
 }
