ae50f4c7a3ef9e194b18c07f09b220622f3e5913
Kyeong Jun <v49011591@gmail.com>
2022-03-03 22:07:17 +0900

    냄새 22. 데이터 클래스     리팩토링 42. 레코드 캡슐화하기

--- src/main/java/me/whiteship/refactoring/_22_data_class/_42_encapsulate_record/Organization.java
+++ src/main/java/me/whiteship/refactoring/_22_data_class/_42_encapsulate_record/Organization.java
@@ -2,8 +2,23 @@ package me.whiteship.refactoring._22_data_class._42_encapsulate_record;
 
 public class Organization {
 
-    public String name;
+    private String name;
 
-    public String country;
+    private String country;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
